5c27eedac759b6b050a5f87b308e96871a22106c
kanchan sharma <kanusharma881388@gmail.com>
2026-05-27 23:15:40 +0530

    feat: implemented modular architecture and global error handling middleware

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import pool from '../config/db.js';
 import { Student } from '../interfaces/student.js';
 
@@ -14,7 +14,8 @@ export const getAllStudents = async (req: Request, res: Response) => {
 };
 
 // 2. Create New Student
-export const createStudent = async (req: Request, res: Response) => {
+
+export const createStudent = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { name, email, age } = req.body;
 
@@ -22,20 +23,11 @@ export const createStudent = async (req: Request, res: Response) => {
       return res.status(400).json({ error: 'Please provide name, email, and age' });
     }
 
-    const queryText = `
-            INSERT INTO students (name, email, age) 
-            VALUES ($1, $2, $3) 
-            RETURNING *;
-        `;
-    const values = [name, email, age];
-    const result = await pool.query<Student>(queryText, values);
+    const queryText = `INSERT INTO students (name, email, age) VALUES ($1, $2, $3) RETURNING *;`;
+    const result = await pool.query<Student>(queryText, [name, email, age]);
 
     res.status(201).json(result.rows[0]);
-  } catch (err: any) {
-    console.error(err.message);
-    if (err.code === '23505') {
-      return res.status(400).json({ error: 'Email already exists!' });
-    }
-    res.status(500).send('Server Error');
+  } catch (err) {
+    next(err); // Yeh error ko seedha errorHandler.ts middleware ke paas bhej dega!
   }
 };
