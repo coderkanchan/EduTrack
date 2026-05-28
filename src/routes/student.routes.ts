e9bb08a8bde880816da00f68a2fb98e44821cc9c
kanchan sharma <kanusharma881388@gmail.com>
2026-05-28 23:43:06 +0530

    feat: integrate zod validation middleware for student creation request body

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import { getAllStudents, createStudent, updateStudent, deleteStudent, getStudentProfileWithStats } from '../controllers/student.controller.js';
-
+import { validate } from '../middlewares/validate.middleware.js';
+import { createStudentSchema } from '../validators/student.validator.js';
 const router = Router();
+
+router.post('/', validate(createStudentSchema), createStudent);
 
 router.get('/', getAllStudents);
 router.post('/', createStudent);
