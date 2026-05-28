375f9723e008a009bdd1cbe9c6a0c1e6b84890d2
kanchan sharma <kanusharma881388@gmail.com>
2026-05-28 19:25:38 +0530

    initial commit

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,12 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-// Explicitly connecting using the direct database URL to bypass engine type issues
 const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
   log: ['query', 'error', 'warn'],
 });
 
