328e889f182670840a8e4ec2b02544df975f2e23
kanchan sharma <kanusharma881388@gmail.com>
2026-05-28 23:29:29 +0530

    initial commit

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -8,12 +8,10 @@ export const errorHandler = (
 ) => {
   console.error('💥 Central Error Logged:', err.message);
 
-  // Database unique constraint error (Email already exists)
   if (err.code === '23505') {
     return res.status(400).json({ error: 'Email already exists!' });
   }
 
-  // Default Fallback Server Error
   res.status(err.status || 500).json({
     error: err.message || 'Internal Server Error',
   });
