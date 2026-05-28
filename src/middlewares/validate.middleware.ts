cc321eace3232256ca9aeb1522ac87bcb2144814
kanchan sharma <kanusharma881388@gmail.com>
2026-05-28 23:41:59 +0530

    feat: integrate zod validation middleware for student creation request body

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -4,21 +4,18 @@ import { AnyZodObject, ZodError } from 'zod';
 export const validate = (schema: AnyZodObject) =>
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      // Yeh line req.body ko check karegi hamare rules ke mutabik
       await schema.parseAsync({
         body: req.body,
         query: req.query,
         params: req.params,
       });
 
-      // Agar sab sahi raha, toh agle controller par bhej do
       next();
     } catch (error) {
       if (error instanceof ZodError) {
-        // Agar Zod ne koi error pakda, toh asaan bhasha mein client ko list return karo
         const errorMessages = error.errors.map((err) => ({
-          field: err.path[1], // Batayega ki kaunse field mein galti hai (e.g., email)
-          message: err.message, // Batayega kya galti hai
+          field: err.path[1], 
+          message: err.message, 
         }));
 
         res.status(400).json({
