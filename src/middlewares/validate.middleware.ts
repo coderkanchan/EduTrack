0db16e29cdf01974062f20bc19a86919cdf77f19
kanchan sharma <kanusharma881388@gmail.com>
2026-06-12 22:26:16 +0530

    initial commit

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -13,7 +13,6 @@ export const validate = (schema: ZodSchema) =>
       next();
     } catch (error) {
       if (error instanceof ZodError) {
-        
         const errorMessages = error.issues.map((err) => {
           return {
             field: err.path[1] || err.path[0] || "unknown",
