339e8be5fd9eecd0d26f70b770fee1117e5aacc1
kanchan sharma <kanusharma881388@gmail.com>
2026-06-21 23:27:56 +0530

    initial commit

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -11,7 +11,6 @@ export const errorHandler = (
   if (err.code === '23505') {
     return res.status(400).json({ error: 'Email already exists!' });
   }
-
   res.status(err.status || 500).json({
     error: err.message || 'Internal Server Error',
   });
