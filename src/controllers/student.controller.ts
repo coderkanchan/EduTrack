ffccda66c05a924ef41f9189b62b070a7bf51ec6
kanchan sharma <kanusharma881388@gmail.com>
2026-07-08 23:18:54 +0530

    initial commit

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -96,7 +96,6 @@ export const deleteStudent = async (req: Request, res: Response, next: NextFunct
 };
 
 export const getStudentProfileWithStats = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-  
   try {
     const { id } = req.params;
     const student = await prisma.student.findUnique({
