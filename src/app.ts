70afdeb953ecd19d89f4ebfa814c88ce38696ca5
kanchan sharma <kanusharma881388@gmail.com>
2026-07-17 22:53:09 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import studentRoutes from './routes/student.routes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
+
 const app = express();
 
 app.use(express.json());
