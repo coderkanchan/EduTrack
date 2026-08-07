d8b1d72ac7bf4151f7104bc6cf3a2c2d765b37cb
kanchan sharma <kanusharma881388@gmail.com>
2026-08-07 23:18:49 +0530

    initial commit

--- src/controllers/student.controller.ts
+++ src/controllers/student.controller.ts
@@ -130,6 +130,7 @@ export const deleteStudent = async (req: Request, res: Response, next: NextFunct
   }
 };
 
+
 export const getStudentProfileWithStats = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const { id } = req.params;
